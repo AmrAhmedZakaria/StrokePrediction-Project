35d0cda64e28f9e6974005b48c4d54d12391225d
AmrAhmedZakaria <aa1378632@gmail.com>
2024-12-09 00:41:05 +0200

    modified app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-model_path = "Models/final.pkl"
+model_path = "final.pkl"
 with open(model_path, "rb") as file:
     model = pickle.load(file)
 
